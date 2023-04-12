c5b72110e71da278a4cdbe7c46d73e7786b08765
gigachad12yearold <nirita011@gmail.com>
2023-04-12 23:23:34 +0200

    re upd web and program + shop catalog

--- src/route/index.js
+++ src/route/index.js
@@ -3251,7 +3251,7 @@ router.get('/shopcatalog', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('shopcatalog', {
-    // layout: 'shop',
+    layout: 'shop',
     navigation: {
       links: [
         {
